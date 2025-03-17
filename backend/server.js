190fd58251be8d2fb2e40ab8bcebd6a20b286871
shubherthi-mitra <shubherthimitra108@gmail.com>
2025-03-17 13:50:56 +0530

    cors updated in backend

--- backend/server.js
+++ backend/server.js
@@ -22,8 +22,8 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "http://127.0.0.1:5173", // User frontend
-      "http://127.0.0.1:5174", // Admin frontend
+      process.env.FRONTEND_URL, // User frontend
+      process.env.ADMIN_URL, // Admin frontend
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
