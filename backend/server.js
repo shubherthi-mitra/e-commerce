00fcd9f1b2fb263677b71d8e4998de6d86097e90
shubherthi-mitra <shubherthimitra108@gmail.com>
2025-03-17 14:19:11 +0530

    slashes removed for conflict.

--- backend/server.js
+++ backend/server.js
@@ -22,14 +22,27 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-      "https://e-commerce-frontend-virid-theta.vercel.app/", // User frontend
-      "https://e-commerce-admin-six-bice.vercel.app/", // Admin frontend
+      "https://e-commerce-frontend-virid-theta.vercel.app", // User frontend
+      "https://e-commerce-admin-six-bice.vercel.app", // Admin frontend
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true, // Allow cookies & auth headers
   })
 );
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", req.headers.origin);
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+
+  next();
+});
 
 //API endpoints
 app.use("/api/user", userRouter);
